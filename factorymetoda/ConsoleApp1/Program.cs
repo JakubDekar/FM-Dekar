4d47165197ee436c364d079b048a242b77731cc3
JakubDekar <94068114+JakubDekar@users.noreply.github.com>
2024-10-31 09:16:33 +0100

    Add files via upload

---
+++ factorymetoda/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography.X509Certificates;
+
+class Zvire{
+    private string zvuk;
+    private string rasa;
+
+    private Zvire(string zvuk, string rasa){
+        this.zvuk = zvuk;
+        this.rasa = rasa;
+    }
+
+    public static Zvire kocka(){
+        return new Zvire("Mnau", "Kocka");
+    }
+
+    public string ZvukKocky(){
+        return zvuk;
+    }
+
+    public string RasaKocky(){
+        return rasa;
+    }
+}
